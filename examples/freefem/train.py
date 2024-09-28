3da864ab1e9ee70628c7d084bf76a16522435ccb
allaffa <massimiliano.lupo.pasini@gmail.com>
2024-09-28 00:11:08 -0400

    train example added for freefem

---
+++ examples/freefem/train.py
@@ -0,0 +1,414 @@
+import os, re
+import json
+import logging
+import sys
+from mpi4py import MPI
+import argparse
+
+import random
+import numpy as np
+
+import torch
+from torch_geometric.data import Data
+
+from torch_geometric.transforms import Distance, Spherical, LocalCartesian
+
+import hydragnn
+from hydragnn.utils.time_utils import Timer
+from hydragnn.utils.model import print_model
+from hydragnn.utils.abstractbasedataset import AbstractBaseDataset
+from hydragnn.utils.distdataset import DistDataset
+from hydragnn.utils.pickledataset import SimplePickleWriter, SimplePickleDataset
+from hydragnn.preprocess.utils import gather_deg
+from hydragnn.preprocess.load_data import split_dataset
+
+import hydragnn.utils.tracer as tr
+from hydragnn.utils.print_utils import iterate_tqdm, log
+
+try:
+    from hydragnn.utils.adiosdataset import AdiosWriter, AdiosDataset
+except ImportError:
+    pass
+
+import subprocess
+from hydragnn.utils import nsplit
+
+
+def info(*args, logtype="info", sep=" "):
+    getattr(logging, logtype)(sep.join(map(str, args)))
+
+
+compute_distance = Distance(norm=False, cat=True)
+compute_local_coordinates = LocalCartesian(norm=False, cat=True)
+
+
+class FreeFem(AbstractBaseDataset):
+    def __init__(self, dirpath, var_config, dist=False):
+        super().__init__()
+
+        self.dist = dist
+        if self.dist:
+            assert torch.distributed.is_initialized()
+            self.world_size = torch.distributed.get_world_size()
+            self.rank = torch.distributed.get_rank()
+
+        global_txt_files_list = [f for f in os.listdir(dirpath+'/freefem') if f.endswith('.txt')]
+
+        if self.dist:
+            local_txt_file_list = list(nsplit(global_txt_files_list, self.world_size))[
+                self.rank
+            ]
+        else:
+            local_txt_file_list = global_txt_files_list
+
+        for filename in local_txt_file_list:
+            data_obj = self.read_txt_to_geometric_data(os.path.join('dataset', 'freefem', filename))
+            self.dataset.append(data_obj)
+
+    def read_txt_to_geometric_data(self, filepath):
+
+        directory = filepath.split('problem')[0]
+
+        problem_id = filepath.split('_')[1].split('.')[0]  # Extract the part between 'problem_' and '.txt'
+
+        # Read the data from the file
+        data = np.loadtxt(filepath, skiprows=1)  # Skip the header row
+
+        # Split the data into position and features
+        pos = torch.tensor(data[:, :2], dtype=torch.float)  # First two columns (Vertex_X, Vertex_Y)
+        x = torch.tensor(data[:, 2:], dtype=torch.float)  # Remaining columns
+
+        # Create the PyTorch Geometric data object
+        data_obj = Data(pos=pos, x=x)
+
+        # Example usage:
+        file_path = 'mesh_'+str(problem_id)+'.msh'
+        elements = self.parse_msh_file(os.path.join(directory, file_path))
+        edge_index = self.create_edge_index(elements)
+
+        # Create graph connectivity
+        data_obj.edge_index = edge_index
+
+        # Add distance as edge attribute
+        data_obj = compute_distance(data_obj)
+
+        # Add local Cartesian coordinates as additional edge attributes
+        data_obj = compute_local_coordinates(data_obj)
+
+        return data_obj
+
+    def parse_msh_file(self, filepath):
+
+        with open(filepath, 'r') as file:
+            lines = file.readlines()
+
+        elements_start = False
+        elements_list = []
+
+        for line in lines:
+            # Split the line into tokens and store the vertex indices
+            tokens = re.split(r'\s+', line.strip())
+            if len(tokens) == 4:  # A valid line should have at least element and 3 vertices
+                elements_list.append([int(v)-1 for v in tokens[:-1]])  # Assuming it's a triangle element
+
+        return elements_list
+
+    def create_edge_index(self, elements_list):
+
+        edge_set = set()
+
+        # Loop over each triangular element to extract edges
+        for element in elements_list:
+            v1, v2, v3 = element
+
+            # Add edges (ensure undirected by adding both directions)
+            edge_set.add((v1, v2))
+            edge_set.add((v2, v1))
+            edge_set.add((v2, v3))
+            edge_set.add((v3, v2))
+            edge_set.add((v3, v1))
+            edge_set.add((v1, v3))
+
+        # Convert the set to a list and then to a PyTorch tensor
+        edge_list = list(edge_set)
+        edge_index = torch.tensor(edge_list, dtype=torch.long).t().contiguous()  # Transpose to shape [2, num_edges]
+
+        return edge_index
+
+    def len(self):
+        return len(self.dataset)
+
+    def get(self, idx):
+        return self.dataset[idx]
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument("--sampling", type=float, help="sampling ratio", default=None)
+    parser.add_argument(
+        "--preonly",
+        action="store_true",
+        help="preprocess only (no training)",
+    )
+    parser.add_argument(
+        "--inputfile", help="input file", type=str, default="freefem.json"
+    )
+    parser.add_argument(
+        "--energy_per_atom",
+        help="option to normalize energy by number of atoms",
+        type=bool,
+        default=True,
+    )
+    parser.add_argument("--ddstore", action="store_true", help="ddstore dataset")
+    parser.add_argument("--ddstore_width", type=int, help="ddstore width", default=None)
+    parser.add_argument("--shmem", action="store_true", help="shmem")
+    parser.add_argument("--log", help="log name")
+    parser.add_argument("--batch_size", type=int, help="batch_size", default=None)
+    parser.add_argument("--everyone", action="store_true", help="gptimer")
+    parser.add_argument("--modelname", help="model name")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(
+        "--adios",
+        help="Adios dataset",
+        action="store_const",
+        dest="format",
+        const="adios",
+    )
+    group.add_argument(
+        "--pickle",
+        help="Pickle dataset",
+        action="store_const",
+        dest="format",
+        const="pickle",
+    )
+    parser.set_defaults(format="adios")
+    args = parser.parse_args()
+
+    graph_feature_names = []
+    graph_feature_dims = []
+    node_feature_names = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
+    node_feature_dims = [1, 1, 1, 1, 1, 1, 1, 1, 1]
+    dirpwd = os.path.dirname(os.path.abspath(__file__))
+    datadir = os.path.join(dirpwd, "dataset")
+    ##################################################################################################################
+    input_filename = os.path.join(dirpwd, args.inputfile)
+    ##################################################################################################################
+    # Configurable run choices (JSON file that accompanies this example script).
+    with open(input_filename, "r") as f:
+        config = json.load(f)
+    verbosity = config["Verbosity"]["level"]
+    var_config = config["NeuralNetwork"]["Variables_of_interest"]
+    var_config["graph_feature_names"] = graph_feature_names
+    var_config["graph_feature_dims"] = graph_feature_dims
+    var_config["node_feature_names"] = node_feature_names
+    var_config["node_feature_dims"] = node_feature_dims
+
+    if args.batch_size is not None:
+        config["NeuralNetwork"]["Training"]["batch_size"] = args.batch_size
+
+    ##################################################################################################################
+    # Always initialize for multi-rank training.
+    comm_size, rank = hydragnn.utils.setup_ddp()
+    ##################################################################################################################
+
+    comm = MPI.COMM_WORLD
+
+    ## Set up logging
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%%(levelname)s (rank %d): %%(message)s" % (rank),
+        datefmt="%H:%M:%S",
+    )
+
+    log_name = "FreeFem" if args.log is None else args.log
+    hydragnn.utils.setup_log(log_name)
+    writer = hydragnn.utils.get_summary_writer(log_name)
+
+    log("Command: {0}\n".format(" ".join([x for x in sys.argv])), rank=0)
+
+    modelname = "FreeFem" if args.modelname is None else args.modelname
+    if args.preonly:
+        ## local data
+        total = FreeFem(
+            os.path.join(datadir),
+            var_config,
+            dist=True,
+        )
+        ## This is a local split
+        trainset, valset, testset = split_dataset(
+            dataset=total,
+            perc_train=0.9,
+            stratify_splitting=False,
+        )
+        print(rank, "Local splitting: ", len(trainset), len(valset), len(testset))
+
+        deg = gather_deg(trainset)
+        config["pna_deg"] = deg
+
+        setnames = ["trainset", "valset", "testset"]
+
+        ## adios
+        if args.format == "adios":
+            fname = os.path.join(
+                os.path.dirname(__file__), "./dataset/%s.bp" % modelname
+            )
+            adwriter = AdiosWriter(fname, comm)
+            adwriter.add("trainset", trainset)
+            adwriter.add("valset", valset)
+            adwriter.add("testset", testset)
+            # adwriter.add_global("minmax_node_feature", total.minmax_node_feature)
+            # adwriter.add_global("minmax_graph_feature", total.minmax_graph_feature)
+            adwriter.add_global("pna_deg", deg)
+            adwriter.save()
+
+        ## pickle
+        elif args.format == "pickle":
+            basedir = os.path.join(
+                os.path.dirname(__file__), "dataset", "%s.pickle" % modelname
+            )
+            attrs = dict()
+            attrs["pna_deg"] = deg
+            SimplePickleWriter(
+                trainset,
+                basedir,
+                "trainset",
+                # minmax_node_feature=total.minmax_node_feature,
+                # minmax_graph_feature=total.minmax_graph_feature,
+                use_subdir=True,
+                attrs=attrs,
+            )
+            SimplePickleWriter(
+                valset,
+                basedir,
+                "valset",
+                # minmax_node_feature=total.minmax_node_feature,
+                # minmax_graph_feature=total.minmax_graph_feature,
+                use_subdir=True,
+            )
+            SimplePickleWriter(
+                testset,
+                basedir,
+                "testset",
+                # minmax_node_feature=total.minmax_node_feature,
+                # minmax_graph_feature=total.minmax_graph_feature,
+                use_subdir=True,
+            )
+        sys.exit(0)
+
+    tr.initialize()
+    tr.disable()
+    timer = Timer("load_data")
+    timer.start()
+
+    if args.format == "adios":
+        info("Adios load")
+        assert not (args.shmem and args.ddstore), "Cannot use both ddstore and shmem"
+        opt = {
+            "preload": False,
+            "shmem": args.shmem,
+            "ddstore": args.ddstore,
+            "ddstore_width": args.ddstore_width,
+        }
+        fname = os.path.join(os.path.dirname(__file__), "./dataset/%s.bp" % modelname)
+        trainset = AdiosDataset(fname, "trainset", comm, **opt, var_config=var_config)
+        valset = AdiosDataset(fname, "valset", comm, **opt, var_config=var_config)
+        testset = AdiosDataset(fname, "testset", comm, **opt, var_config=var_config)
+    elif args.format == "pickle":
+        info("Pickle load")
+        basedir = os.path.join(
+            os.path.dirname(__file__), "dataset", "%s.pickle" % modelname
+        )
+        trainset = SimplePickleDataset(
+            basedir=basedir, label="trainset", var_config=var_config
+        )
+        valset = SimplePickleDataset(
+            basedir=basedir, label="valset", var_config=var_config
+        )
+        testset = SimplePickleDataset(
+            basedir=basedir, label="testset", var_config=var_config
+        )
+        # minmax_node_feature = trainset.minmax_node_feature
+        # minmax_graph_feature = trainset.minmax_graph_feature
+        pna_deg = trainset.pna_deg
+        if args.ddstore:
+            opt = {"ddstore_width": args.ddstore_width}
+            trainset = DistDataset(trainset, "trainset", comm, **opt)
+            valset = DistDataset(valset, "valset", comm, **opt)
+            testset = DistDataset(testset, "testset", comm, **opt)
+            # trainset.minmax_node_feature = minmax_node_feature
+            # trainset.minmax_graph_feature = minmax_graph_feature
+            trainset.pna_deg = pna_deg
+    else:
+        raise NotImplementedError("No supported format: %s" % (args.format))
+
+    info(
+        "trainset,valset,testset size: %d %d %d"
+        % (len(trainset), len(valset), len(testset))
+    )
+
+    if args.ddstore:
+        os.environ["HYDRAGNN_AGGR_BACKEND"] = "mpi"
+        os.environ["HYDRAGNN_USE_ddstore"] = "1"
+
+    (train_loader, val_loader, test_loader,) = hydragnn.preprocess.create_dataloaders(
+        trainset, valset, testset, config["NeuralNetwork"]["Training"]["batch_size"]
+    )
+
+    config = hydragnn.utils.update_config(config, train_loader, val_loader, test_loader)
+    ## Good to sync with everyone right after DDStore setup
+    comm.Barrier()
+
+    hydragnn.utils.save_config(config, log_name)
+
+    timer.stop()
+
+    model = hydragnn.models.create_model_config(
+        config=config["NeuralNetwork"],
+        verbosity=verbosity,
+    )
+    model = hydragnn.utils.get_distributed_model(model, verbosity)
+
+    # Print details of neural network architecture
+    print_model(model)
+
+    learning_rate = config["NeuralNetwork"]["Training"]["Optimizer"]["learning_rate"]
+    optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+        optimizer, mode="min", factor=0.5, patience=5, min_lr=0.00001
+    )
+
+    hydragnn.utils.load_existing_model_config(
+        model, config["NeuralNetwork"]["Training"], optimizer=optimizer
+    )
+
+    ##################################################################################################################
+
+    hydragnn.train.train_validate_test(
+        model,
+        optimizer,
+        train_loader,
+        val_loader,
+        test_loader,
+        writer,
+        scheduler,
+        config["NeuralNetwork"],
+        log_name,
+        verbosity,
+        create_plots=False,
+        compute_pde_pinn=True
+    )
+
+    hydragnn.utils.save_model(model, optimizer, log_name)
+    hydragnn.utils.print_timers(verbosity)
+
+    if tr.has("GPTLTracer"):
+        import gptl4py as gp
+
+        eligible = rank if args.everyone else 0
+        if rank == eligible:
+            gp.pr_file(os.path.join("logs", log_name, "gp_timing.p%d" % rank))
+        gp.pr_summary_file(os.path.join("logs", log_name, "gp_timing.summary"))
+        gp.finalize()
+    sys.exit(0)
