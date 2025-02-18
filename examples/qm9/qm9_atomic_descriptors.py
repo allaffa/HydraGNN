871ee2102243a6fedd5cf2e0537b79c07576b6bd
allaffa <massimiliano.lupo.pasini@gmail.com>
2025-02-18 14:51:01 -0500

    atomic descriptors files added

---
+++ examples/qm9/qm9_atomic_descriptors.py
@@ -0,0 +1,203 @@
+import os
+import pdb
+import json
+import torch
+import torch_geometric
+from torch_geometric.transforms import AddLaplacianEigenvectorPE
+import argparse
+
+# deprecated in torch_geometric 2.0
+
+try:
+    from torch_geometric.loader import DataLoader
+except ImportError:
+    from torch_geometric.data import DataLoader
+
+import hydragnn
+
+from hydragnn.utils.descriptors_and_embeddings.atomicdescriptors import (
+    atomicdescriptors,
+)
+
+from hydragnn.utils.print.print_utils import print_distributed, iterate_tqdm
+
+num_samples = 1000
+
+
+atomicdescriptor = atomicdescriptors(
+    embeddingfilename="./embedding.json",
+    overwritten=True,
+    element_types=["C", "H", "O", "N", "F", "S"],
+    one_hot=False,
+)
+
+from generate_dictionaries_pure_elements import (
+    generate_dictionary_elements,
+)
+
+periodic_table = generate_dictionary_elements()
+
+# Update each sample prior to loading.
+def qm9_pre_transform(data, transform):
+    # LPE
+    data = transform(data)
+    # Set descriptor as element type.
+    data.x = data.z.float().view(-1, 1)
+    # Only predict free energy (index 10 of 19 properties) for this run.
+    data.y = data.y[:, 10] / len(data.x)
+    graph_features_dim = [1]
+    node_feature_dim = [1]
+    # gps requires relative edge features, introduced rel_lapPe as edge encodings
+    source_pe = data.pe[data.edge_index[0]]
+    target_pe = data.pe[data.edge_index[1]]
+    data.rel_pe = torch.abs(source_pe - target_pe)  # Compute feature-wise difference
+    return data
+
+
+def add_atomic_descriptors(data):
+
+    descriptor_tensor = torch.empty((data.x.shape[0], 18))
+
+    for atom_id in range(data.x.shape[0]):
+
+        atomic_string = periodic_table[int(data.x[atom_id, 0].item())]
+
+        descriptor_tensor[atom_id, :] = atomicdescriptor.get_atom_features(
+            atomic_string
+        )
+        data.x = torch.cat([data.x, descriptor_tensor], dim=1)
+
+    return data
+
+
+def qm9_pre_filter(data):
+    return data.idx < num_samples
+
+
+def main(mpnn_type=None, global_attn_engine=None, global_attn_type=None):
+    # FIX random seed
+    random_state = 0
+    torch.manual_seed(random_state)
+
+    # Set this path for output.
+    try:
+        os.environ["SERIALIZED_DATA_PATH"]
+    except KeyError:
+        os.environ["SERIALIZED_DATA_PATH"] = os.getcwd()
+
+    # Configurable run choices (JSON file that accompanies this example script).
+    filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), "qm9.json")
+    with open(filename, "r") as f:
+        config = json.load(f)
+
+    # If a model type is provided, update the configuration accordingly.
+    if global_attn_engine:
+        config["NeuralNetwork"]["Architecture"][
+            "global_attn_engine"
+        ] = global_attn_engine
+
+    if global_attn_type:
+        config["NeuralNetwork"]["Architecture"]["global_attn_type"] = global_attn_type
+
+    if mpnn_type:
+        config["NeuralNetwork"]["Architecture"]["mpnn_type"] = mpnn_type
+
+    verbosity = config["Verbosity"]["level"]
+    var_config = config["NeuralNetwork"]["Variables_of_interest"]
+
+    # Always initialize for multi-rank training.
+    world_size, world_rank = hydragnn.utils.distributed.setup_ddp()
+
+    log_name = f"qm9_test_{mpnn_type}" if mpnn_type else "qm9_test"
+    # Enable print to log file.
+    hydragnn.utils.print.print_utils.setup_log(log_name)
+
+    # LPE
+    transform = AddLaplacianEigenvectorPE(
+        k=config["NeuralNetwork"]["Architecture"]["pe_dim"],
+        attr_name="pe",
+        is_undirected=True,
+    )
+
+    # Use built-in torch_geometric datasets.
+    # Filter function above used to run quick example.
+    # NOTE: data is moved to the device in the pre-transform.
+    # NOTE: transforms/filters will NOT be re-run unless the qm9/processed/ directory is removed.
+    dataset = torch_geometric.datasets.QM9(
+        root="dataset/qm9",
+        pre_transform=lambda data: qm9_pre_transform(data, transform),
+        pre_filter=qm9_pre_filter,
+    )
+
+    print_distributed(2, "Add atomic descriptors")
+    dataset = [
+        add_atomic_descriptors(data)
+        for data in iterate_tqdm(dataset, verbosity_level=2)
+    ]
+
+    train, val, test = hydragnn.preprocess.split_dataset(
+        dataset, config["NeuralNetwork"]["Training"]["perc_train"], False
+    )
+
+    (train_loader, val_loader, test_loader,) = hydragnn.preprocess.create_dataloaders(
+        train, val, test, config["NeuralNetwork"]["Training"]["batch_size"]
+    )
+
+    config = hydragnn.utils.input_config_parsing.update_config(
+        config, train_loader, val_loader, test_loader
+    )
+
+    model = hydragnn.models.create_model_config(
+        config=config["NeuralNetwork"],
+        verbosity=verbosity,
+    )
+    model = hydragnn.utils.distributed.get_distributed_model(model, verbosity)
+
+    learning_rate = config["NeuralNetwork"]["Training"]["Optimizer"]["learning_rate"]
+    optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+        optimizer, mode="min", factor=0.5, patience=5, min_lr=0.00001
+    )
+
+    # Run training with the given model and qm9 datasets.
+    writer = hydragnn.utils.model.model.get_summary_writer(log_name)
+    hydragnn.utils.input_config_parsing.save_config(config, log_name)
+
+    hydragnn.train.train_validate_test(
+        model,
+        optimizer,
+        train_loader,
+        val_loader,
+        test_loader,
+        writer,
+        scheduler,
+        config["NeuralNetwork"],
+        log_name,
+        verbosity,
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Run the QM9 example with optional model type."
+    )
+    parser.add_argument(
+        "--mpnn_type",
+        type=str,
+        default=None,
+        help="Specify the model type for training (default: None).",
+    )
+    parser.add_argument(
+        "--global_attn_engine",
+        type=str,
+        default=None,
+        help="Specify if global attention is being used (default: None).",
+    )
+    parser.add_argument(
+        "--global_attn_type",
+        type=str,
+        default=None,
+        help="Specify the global attention type (default: None).",
+    )
+    args = parser.parse_args()
+    main(mpnn_type=args.mpnn_type)
